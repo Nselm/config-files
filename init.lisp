2a9bb896032fed4bec160d6dbb6002ca8a1eea1f
Nselm <90971536+Nselm@users.noreply.github.com>
2022-09-06 17:49:31 +0000

    Add files via upload

--- init.lisp
+++ init.lisp
@@ -1,150 +1,154 @@
-(defvar *odysseus* (make-keymap "my-map"))
+;;; general utilites
+(defun empty-string-p (str)
+  (equal str ""))
+(set-prefix-key (kbd "s-d"))
 
-(define-key *odysseus*
-  "C-f" 'nyxt/web-mode:history-forwards
-  "C-b" 'nyxt/web-mode:history-backwards
-  "C-s a" 'nyxt/web-mode:search-buffers
-  )
+;;; Init stuff
+(run-shell-command "xsetroot -cursor_name left_ptr")
 
-(define-bookmarklet-command change-yt-speed
-  "Change playback speed of yt videos"
-  "(function (){
-const rate = prompt('Enter playback Rate');
-if (rate!=null){
-const video = document.getElementsByTagName('video')[0];
-video.playbackRate = parseFloat(rate);
-}})()")
+(defun filter-batt-status (battery-info kind)
+  (multiple-value-bind (_ info)
+      (cl-ppcre:scan-to-strings " ([A-Za-z]+), ([0-9]+%)" battery-info)
+    (ecase kind
+      (status (aref info 0))
+      (power (aref info 1)))))
 
-(define-bookmarklet-command zapp-fcking-consent
-  "Zapp away googles **** consent poput."
-"(function (){
-    let buttons = document.getElementsByTagName('ytd-button-renderer');
-    let len = buttons.length;
-    let zappIt = buttons[len-1];
-    zappIt.click();
-})();
-")
+(defcommand battery () ()
+  "Show battery status as seen through acpi"
+  (message (uiop:run-program "acpi" :output :string)))
 
-(defun use-theme (theme)
-  (funcall theme))
-(defun make-define (pair)
-  (let ((place (car pair))
-	(css (cadr pair)))
-    `(define-configuration ,place
-	 ((style (str:concat
-		  %slot-default%
-		  (cl-css:css
-		   ',css)))))))
+(defun show-batt ()
+  (filter-batt-status (uiop:run-program "acpi" :output :string)
+		      'power))
 
-(defmacro define-theme (name place-css-pairs)
-  `(defvar ,name (lambda ()
-		   ,@(mapcar #'(lambda (pair)
-				(make-define pair))
-			    place-css-pairs))))
-
-
-(define-theme my-theme
-    ((prompt-buffer  ((body
-		       :background-color "black"
-		       :color "#808080")
-	      ("#prompt-area"
-		       :background-color "black")
-		      ;; The area you input text in.
-		      ("#input"
-		       :background-color "white")
-		      (".source-name"
-		       :color "black"
-		       :background-color "gray")
-		      (".source-content"
-		       :background-color "black")
-		      (".source-content th"
-		       :border "1px solid lightgray"
-		       :background-color "black")
-		     ;; The currently highlighted option.
-		      ("#selection"
-		       :background-color "#37a8e4"
-		       :color "white")
-		      (.marked :background-color "darkgray"
-			       :font-weight "bold"
-			       :color "white")
-		      (.selected :background-color "black"
-				 :color "white")))
-
-     ((panel-buffer internal-buffer) 
-      ((body :background-color "black"
-	     :color "lightgray")
-	     (hr
-	      :color "darkgray")
-	     (a
-	      :color "lightgray")
-	     (.button
-	      :color "lightgray"
-	      :background-color "gray")))))
-
-(use-theme my-theme)
-
-
-;(defun eval-in-emacs (&rest s-exps)
-;  "Evaluate S-EXPS with emacsclient."
-;  (let ((s-exps-string (cl-strings:replace-all
-;			(write-to-string
-;			 `(progn ,@s-exps) :case :downcase)
-;			;; Discard the package prefix.
-;			"nyxt::" "")))
-;    (format *error-output* "Sending to Emacs:~%~a~%" s-exps-string)
-;    (uiop:run-program
-;     (list "emacsclient" "--eval" s-exps-string)))))
-
-(define-configuration password:keepassxc-interface
-  ((password:password-file "/home/whysoserious/Passwords.kdbx")))
-
-(define-configuration buffer
-  ((password-interface (make-instance 'password:user-keepassxc-interface))))
-
-(DEFINE-CONFIGURATION BROWSER
-  ((EXTERNAL-EDITOR-PROGRAM "/usr/bin/emacsclient")))
-
-
-(defvar *redirect-list*
-  '(("www.twitter.com" . "www.nitter.net")))
-
-(defun find-redirect (query-url)
-  (assoc (quri:uri-host query-url) *redirect-list* :test #'equal))
-(defun redirection (redirect-pair)
-  (cdr redirect-pair))
-
-(defun redirector (request)
-  (let ((query-url (url request)))
-	    (echo query-url)
-    (setf (url request)
-	  (let ((matching-redirect (find-redirect query-url)))
-	    (log:info "Host is ~s" (quri:uri-host query-url))
-	    (when matching-redirect
-	      (log:info "Switching to ~s" (redirection matching-redirect))
-	      (setf (quri:uri-host query-url) (redirection matching-redirect)))
-	    query-url))
-    request))
+(defcommand battery-status (kind)
+  ((:string "status/power ==> "))
+  "Show battery status by acpi, but filtered for the most relevant info"
+  (message (filter-batt-status (uiop:run-program "acpi" :output :string)
+			       (read-from-string kind))))
 
 
 
+(defcommand default-set-up ()  ()
+  (grename "Browse")
+  (alacritty)
+  (emacs)
+  (gnew "Edit"))
+;(setf *mouse-focus-policy* :click)
 
-(define-command youtube-dl-current-page ()
-  "Download yt video in currently open buffer"
-  (let ((url (buffer-get-url)))
-    (uiop:run-program (list "youtube-dl" url))))
-
-(define-configuration web-buffer
-    ((request-resource-hook
-      (hooks:add-hook %slot-default% (make-handler-resource #'redirector)))))
+;(defcommand setup-env () ()
+;  "Setup my default environment"
+;  )
 
 
-(define-mode penellope ()
-  ((keymap-scheme (keymap:make-scheme
-		   scheme:cua *odysseus*
-		   scheme:vi-normal *odysseus*
-		   scheme:emacs *odysseus*))))
+(defcommand firefox () ()
+  (run-shell-command "firefox https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book.html"))
+(defcommand my-emacs () ()
+  (run-shell-command "emacs"))
+(defcommand alacritty () ()
+  (run-shell-command "alacritty"))
 
-(define-configuration (buffer web-buffer)
-  ((default-modes (append '(penellope emacs-mode) %slot-default%))))
+(defcommand nyxt () ()
+  (run-shell-command "nyxt/nyxt"))
 
-		   
+(defun define-multiple-keys (map pair-lst)
+  (mapcar #'(lambda (kbd-pair)
+	      (define-key map (kbd (car kbd-pair)) (cdr kbd-pair)))
+	  pair-lst))
+
+;;; i3w emulation
+#|
+(defparameter *split-variants* '(vsplit hsplit))
+(defparameter *how-to-split* 0)
+
+(defcommand start-in-new-frame (application)
+    ((:string "Enter application to start"))
+  (progn
+    (funcall (nth *how-to-split* *split-variants*))
+    (run-shell-command application)))
+(defcommand split-dependent-on-settings () ()
+  "Splits i3-like depending on settings vertically or horizontally"
+  (funcall (elt *split-variants* *how-to-split*))
+  (fnext)
+  (run-shell-command "alacritty"))
+
+(defcommand choose-vertical-split () ()
+  "Enable vertical split for all further operations of split-dependent-on-settings"
+  (setf *how-to-split* 0))
+(defcommand choose-horizontal-split () ()
+  "Enable vertical split for allefurther operations of split-dependent-on-settings"
+  (setf *how-to-split* 1))
+
+(defcommand kill-application-and-refocus () ()
+  "Kill application in current frame and restore frame split"
+  (delete-window)
+  (remove-split))
+
+(define-multiple-keys *top-map*
+  '(("s-h" . "move-focus left")
+    ("s-j" . "move-focus down")
+    ("s-k" . "move-focus up")
+    ("s-l" . "move-focus right")
+    ("s-RET" . "split-dependent-on-settings")
+    ("s-V" . "choose-vertical-split")
+    ("s-H" . "choose-horizontal-split")
+    ("s-Q" . "kill-application-and-refocus")))
+|#
+
+
+;; For now, I will try stumps approch
+;;; But a few simplifications are in order
+
+(define-multiple-keys *top-map*
+    '(("s-TAB" . "fnext")
+    ;;; This is supposed to mean Super-Shift-Tab
+      ))
+
+
+(defcommand confirm-poweroff (y-no) ((:string "Really want to poweroff"))
+  (if (or (equal y-no "y")
+	  (equal y-no "yes"))
+      (run-shell-command "poweroff")))
+
+(define-multiple-keys *root-map*
+    '(("o" . "only")
+      ("s-a" . "start-application")
+      ("s-c" . "alacritty")
+      ("s-e" . "emacs")
+      ("s-f" . "firefox")
+      ("s-n" . "nyxt")
+      ("s-p" . "confirm-poweroff")  
+      ("s-r" . "loadrc")
+      ("s-s" . "exec systemctl suspend")))
+
+(defcommand start-application () ()
+  (select-window "emacs")
+  (run-shell-command "emacsclient -ne '(call-interactively (quote cr/launcher))'"))
+
+(defcommand do-start-ff () ()
+  (run-shell-command "firefox d")
+  (other-window)
+  (title "firefox"))
+
+(defcommand change-brightness (amount) ((:number "CB:"))
+  (if amount
+      (if (or (< amount 6)
+	      (> amount 100))
+	  (error (format nil "Brightness level invalid ==> ~D" amount))
+	  (run-shell-command (format nil "xbacklight -set ~D" amount)))
+      (run-shell-command "xbacklight -set 10")))
+
+
+(setf *window-format* "%m%n%s%c")
+;; Do NOT use any number above 7, since it will crash stumpwm
+(setf *screen-mode-line-format* (list
+				 "^01^B[^B%n^b] %W^>^01^B"
+				 '(:eval (show-batt))
+				 " %d"))
+(setf *time-modeline-string* "%a %b %e %k:%M")
+(setf *mode-line-timeout* 2)
+(enable-mode-line (current-screen) (current-head) t)
+
+
+
+
